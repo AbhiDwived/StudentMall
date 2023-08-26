4cf6e1fbf215bd227982f4e46b63cd47e5ab5b25
Abhinandan Dhar Duivedi <139725697+AbhiDwived@users.noreply.github.com>
2023-08-26 23:58:20 +0530

    Student Mall

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const dotenv = require("dotenv")
+const cors = require("cors")
+const path = require("path")
+
+dotenv.config()
+
+const router = require("./Routes/index")
+
+require("./dbConnect")
+const app = express()
+app.use(cors({
+    origin: '*'
+}));
+
+app.use("/public", express.static("public"))
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.use(express.json())
+app.use("/api",router)
+
+
+app.use('*',express.static(path.join(__dirname, 'build')));
+var port = process.env.PORT || 8000
+app.listen(port,()=>console.log("Server is Running"))
