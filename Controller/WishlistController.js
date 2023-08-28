0418637697df7f78018bd1f3323454a335872fcd
Abhinandan Dhar Duivedi <139725697+AbhiDwived@users.noreply.github.com>
2023-08-28 21:14:19 +0530

    Retry

--- Controller/WishlistController.js
+++ Controller/WishlistController.js
@@ -23,7 +23,6 @@ async function createWishlist(req, res) {
         else
             res.status(500).send({ result: "Fail", message: "Internal Server Error!!!" })
     }
-    
 }
 async function getAllWishlist(req, res) {
     try {
--- dbConnect.js
+++ dbConnect.js
@@ -4,9 +4,9 @@ async function getConnect(){
     try {
         // await mongoose.connect("mongodb://127.0.0.1:27017/2pmWdMernServer")
         await mongoose.connect(process.env.DBKEY)
-        
+        console.log("Server is Connected!!!")
     } catch (error) {
-        
+        console.log(error)
     }
 }
 getConnect()
